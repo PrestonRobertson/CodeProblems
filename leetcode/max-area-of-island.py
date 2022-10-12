054213da2f77444a062cf28d0845b635da19bdad
Preston Robertson <prestonwilliamrobertson@gmail.com>
2022-10-12 13:10:13 -0500

    695: add basic traversal

--- leetcode/max-area-of-island.py
+++ leetcode/max-area-of-island.py
@@ -1,3 +1,5 @@
+## https://leetcode.com/problems/max-area-of-island/
+## 695. Max Area of Island
 ## You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
 ## The area of an island is the number of cells with a value 1 in the island.
 ## Return the maximum area of an island in grid. If there is no island, return 0.
@@ -6,15 +8,31 @@ from typing import List
 
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        columns = len(grid[0])
-        ys = list(range(0, rows))
-        xs = list(range(0, columns))
+        unvisited = [(0, 0)]
+        visited = []
+        maxArea = 0
 
-        print(ys)
-        print(xs)
+        def addUnvisited(x, y):
+            if (y >= 0 and y < len(grid) and x >= 0 and x < len(grid[0])):
+                if (x, y) not in visited:
+                    unvisited.append((x, y))
 
-        return 0
+        while (len(unvisited) > 0):
+            (x, y) = unvisited.pop()
+
+            print((x, y))
+
+            if (grid[y][x] == 1):
+                print("found an island!")
+            else:
+                addUnvisited(x, y + 1)
+                addUnvisited(x + 1, y)
+                addUnvisited(x, y - 1)
+                addUnvisited(x - 1, y)
+
+            visited.append((x, y))
+
+        return maxArea    
 
 grid = [
     [0,0,1,0,0,0,0,1,0,0,0,0,0],
